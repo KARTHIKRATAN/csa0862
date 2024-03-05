fae12750a27478c44a113cbd1bab84631b83e352
KARTHIKRATAN <154490303+KARTHIKRATAN@users.noreply.github.com>
2024-03-05 13:54:19 +0530

    Create share market.py

---
+++ share market.py
@@ -0,0 +1,14 @@
+prices=eval(input("enter the trade:"))
+if not prices:
+        max_profit=0
+else:
+    first_buy=second_buy=float('inf')
+    first_profit=second_profit=0
+    for price in prices:
+        first_buy=min(first_buy,price)
+        first_profit=max(first_profit,price- first_buy)
+        second_buy=min(second_buy,price-first_profit)
+        second_profit=max(second_profit,price-second_buy)
+
+    max_profit=second_profit
+print(max_profit)
